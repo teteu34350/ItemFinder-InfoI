692c1b4a8a05622fcea1cace5d849aa537f70a5c
Matheus Santana <136133837+teteu34350@users.noreply.github.com>
2025-11-18 09:22:02 -0300

    Delete ItemFinder-InfoI directory

--- ItemFinder-InfoI/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(ItemPerdido)
-admin.site.register(ItemAchado)
-
--- ItemFinder-InfoI/app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.shortcuts import render,redirect,get_object_or_404
-from django.views import View
-
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
